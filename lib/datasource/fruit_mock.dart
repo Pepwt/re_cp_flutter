6e7d50c59d806bc71815abf2d77edeb9cc26a560
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-26 20:55:18 -0300

    first commit

---
+++ lib/datasource/fruit_mock.dart
@@ -0,0 +1,10 @@
+import 'package:fruitstore/domain/fruit.dart';
+
+final fruitMock =  [
+  Fruit("Laranja", "R\$ 10,00"),
+  Fruit("Limão", "R\$ 5,00"),
+  Fruit("Banana", "R\$ 10,00"),
+  Fruit("Abacate", "R\$ 10,00"),
+  Fruit("Laranja", "R\$ 10,00"),
+  Fruit("Laranja", "R\$ 10,00"),
+];
